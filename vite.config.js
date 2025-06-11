af1fcb4824f7b69b0db929aa487b1307b0402c03
saiKiran-ambati <saikiranreddyambati@gmail.com>
2025-06-11 12:28:52 +0530

    uild

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
-// eslint-disable-next-line no-undef
-module.exports = {
-  darkMode: 'class',
-  content: ["./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-
-
-
-
-
-
+  plugins: [react()],
+});
