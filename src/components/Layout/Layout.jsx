b4a3ea8aac2f158562b1f15e2766ca1e23584f20
saiKiran-ambati <saikiranreddyambati@gmail.com>
2025-06-11 12:39:11 +0530

    layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,31 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, useLocation } from 'react-router-dom';
-import Routers from '../Routers/Routers';
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
+import React from 'react'
+import Routers from '../Routers/Routers'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
 
-const LayoutContent = () => {
-  const location = useLocation();
+function Layout() {
+    return (
+        <Router>
+            <Header/>
+            
+            
+            <Routers/>
+            <Footer/>
+        </Router>
+    )
+}
 
-  // Pages that should NOT show Header and Footer
-  const authPages = ['/login', '/signup'];
-  const isAuthPage = authPages.includes(location.pathname);
-
-  return (
-    <>
-      {!isAuthPage && <Header />}
-      <Routers />
-      {!isAuthPage && <Footer />}
-    </>
-  );
-};
-
-const Layout = () => {
-  return (
-    <Router>
-      <LayoutContent />
-    </Router>
-  );
-};
-
-export default Layout;
+export default Layout
